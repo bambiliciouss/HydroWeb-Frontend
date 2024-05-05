51df6e219d7168d5efb8f518d6481336b312fcff
bambiliciouss <ninapotmendoza917@gmail.com>
2024-05-05 22:37:36 +0800

    New backend

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,7 @@
 import {io} from 'socket.io-client'
 
-const socket = io.connect('https://treatsdelight-crownprincess-backend.e6fies.easypanel.host')
+// const socket = io.connect('https://treatsdelight-crownprincess-backend.e6fies.easypanel.host')
+thesis-backend-phi.vercel.app
+const socket = io.connect('thesis-backend-phi.vercel.app')
 
 export default socket;
