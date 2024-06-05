33c11e1ffd0cb647800cfce19996b06a4043d012
bambiliciouss <ninapotmendoza917@gmail.com>
2024-06-05 09:52:51 +0800

    order success

--- src/components/order/VerifyOrderDetails.js
+++ src/components/order/VerifyOrderDetails.js
@@ -4,30 +4,50 @@ import { useDispatch } from "react-redux";
 import { Button, Container, Row, Col } from "reactstrap";
 import { verifyOrders, clearErrors } from "../../actions/orderActions";
 // import { clearCart } from "../../actions/cartActions";
-
+import { Button } from "reactstrap";
 const VerifyOrderDetails = () => {
-    const dispatch = useDispatch();
-    let { id, token } = useParams();
+  const dispatch = useDispatch();
+  let { id, token } = useParams();
 
-    useEffect(() => {
-        dispatch(verifyOrders(token, id));
-        // dispatch(clearCart());
-    }, [dispatch, id, token]);
+  useEffect(() => {
+    dispatch(verifyOrders(token, id));
+    // dispatch(clearCart());
+  }, [dispatch, id, token]);
 
-    return (
-        <div className="bg-zinc-100 min-h-screen d-flex justify-content-center align-items-center">
-            <Container className="bg-white p-4 d-flex flex-column align-items-center rounded-xl mb-20" style={{ maxWidth: "500px" }}>
-                <h2 className="mt-4">Payment Successful!</h2>
-                <p className="text-zinc-500">Payment successful!</p>
-                <Link to="/orders/me">
-                    <Button color="danger" size="lg" block>
-                        Check Orders
-                    </Button>
-                </Link>
-            </Container>
-        </div>
-    );
+  return (
+    // <div className="bg-zinc-100 min-h-screen d-flex justify-content-center align-items-center">
+    //     <Container className="bg-white p-4 d-flex flex-column align-items-center rounded-xl mb-20" style={{ maxWidth: "500px" }}>
+    //         <h2 className="mt-4">Payment Successful!</h2>
+    //         <p className="text-zinc-500">Payment successful!</p>
+    //         <Link to="/orders/me">
+    //             <Button color="danger" size="lg" block>
+    //                 Check Orders
+    //             </Button>
+    //         </Link>
+    //     </Container>
+    // </div>
+
+    <div className="row justify-content-center">
+      <div className="col-6 mt-5 text-center">
+        <img
+          className="my-5 img-fluid d-block mx-auto"
+          src="/images/verify.png"
+          alt="Order Success"
+          width="200"
+          height="200"
+          style={{
+            animation: "fade-in 1s ease-in",
+          }}
+        />
+
+        <h2>Payment Successful!</h2>
+
+        <Button block color="info" onClick={navigate("/ordersummary")}>
+          View Orders
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default VerifyOrderDetails;
-
